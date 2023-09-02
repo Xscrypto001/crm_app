ed91c1d8f7561789db61b9fd1976af0bdf319f79
Peter ikinya <113276612+Xscrypto001@users.noreply.github.com>
2023-09-02 14:40:34 +0300

    done

---
+++ new.jsx
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>project nimbus</title>
+</head>
+
+
+    <body>
+
+      <script  type='text/babel'>
+          Class header extends React.Component{
+
+             render(){
+
+        render(
+        
+              <div ClassName='header'>
+                <div ClassName='profile'></div>
+                 <div ClassName='description'></div>
+              
+              
+              
+              
+              </div>
+
+
+
+
+        )
+
+          }
+
+
+
+
+
+
+      </script>
+
+
+
+       </body>
+
+
+
+
+
+
+
+
+
+</html>
